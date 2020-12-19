f177f5aada11105bb847905354994746668d0914
Mariana Bastos <maribastossrodrigues@gmail.com>
2020-12-19 17:05:26 -0300

    chore: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-module.export = {
+/* eslint-disable linebreak-style */
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    '.+\\.ts$': 'ts-jest',
+  },
 };
