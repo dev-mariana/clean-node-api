5da8238082e1904252ba611367e86be632595ddd
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-02-07 18:17:15 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { Controller } from './../protocols/controller';
 import { MissingParamError } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamError } from '../errors/invalid-param-error';
-import { ServerError } from '../errors/server-error';
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;
@@ -17,22 +16,19 @@ export class SignUpController implements Controller {
         try {
             const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
-        for(const field of requiredFields) {
-            if(!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field));
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field));
+                }
             }
-        }
 
-        const isValid = this.emailValidator.isValid(httpRequest.body.email);
+            const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
-        if(!isValid) {
-            return badRequest(new InvalidParamError('email'));
-        }
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'));
+            }
         } catch (error) {
-            return {
-                statusCode: 500,
-                body: new ServerError()
-            }
+            return serverError();
         }
     };
 }
