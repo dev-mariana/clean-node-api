8964afefc0a9ac5f56c85209bab13f1d1f793dc0
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-02-18 11:14:50 -0300

    refactor: change account result insertedId

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,10 +7,8 @@ export class AccountMongoRepository implements AddAccountRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts');
         const result = await accountCollection.insertOne(accountData);
-        const { insertedId: id } = result;
-        const accountById = await accountCollection.findOne({ _id: id });
-        const { _id, ...accountWithoutId } = accountById;
-        const account = Object.assign({}, accountWithoutId, { id: _id.toHexString() }) as AccountModel;
+        const account = { id: result.insertedId.toString(), ...accountData };
+        console.log(account);
 
         return account;
     }
