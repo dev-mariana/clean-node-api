543b4523efa817a93f76fc0d0b83b30254820d5a
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-03-24 16:27:53 -0300

    feat: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { EmailValidator } from './../../protocols/email-validator';
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import { badRequest, serverError, unauthorized } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { Authentication } from '../../../domain/usecases/authentication';
 
@@ -22,7 +22,7 @@ export class LoginController implements Controller {
                     return badRequest(new MissingParamError(field));
                 }
             }
-            
+
             const { email, password } = httpRequest.body;
             const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
@@ -30,7 +30,12 @@ export class LoginController implements Controller {
                 return badRequest(new InvalidParamError('email'));
             }
 
-            await this.authentication.auth(email, password);
+            const accessToken = await this.authentication.auth(email, password);
+
+            if(!accessToken) {
+                return unauthorized();
+            }
+            
         } catch (error) {
             return serverError(error);
         }
