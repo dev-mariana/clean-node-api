2a3d740d079a706e7c237fc36a4a23a4b28c96af
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-03-24 16:42:46 -0300

    refactor: group LoginController dependencies in a single file

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,6 @@
-import { EmailValidator } from './../../protocols/email-validator';
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, serverError, unauthorized } from "../../helpers/http-helper";
-import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { Authentication } from '../../../domain/usecases/authentication';
+import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from "./login-protocols";
 
 export class LoginController implements Controller {
     private readonly emailValidator: EmailValidator;
@@ -35,7 +33,7 @@ export class LoginController implements Controller {
             if(!accessToken) {
                 return unauthorized();
             }
-            
+
         } catch (error) {
             return serverError(error);
         }
